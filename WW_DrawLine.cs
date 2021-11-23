ce7acfec6bb260631e28db8f5030febe3408d718
White Wolf Game Studio <58172779+WhiteWolfStudio@users.noreply.github.com>
2021-11-23 03:29:24 +0100

    Update WW_DrawLine.cs

--- WW_DrawLine.cs
+++ WW_DrawLine.cs
@@ -13,26 +13,88 @@ namespace WhiteWolf {
 
         public GameObject start;
         public GameObject end;
+
         [Range( 0, 1 )]
         public float interval = 0;
         public Color color;
 
     }
 
+    [System.Serializable]
+    public class AutoDraw {
+
+        public string name;
+
+        [Space]
+
+        public GameObject start;
+        public string end;
+
+        [Range( 0, 1 )]
+        public float interval = 0;
+        public Color color;
+
+        [Space]
+
+
+        [Space]
+
+        public float dis;
+        public Color disColor;
+
+    }
+
     public class WW_DrawLine : MonoBehaviour {
 
+        public bool auto;
+
+        [Space]
+
         public StartEnd[] startEnd;
+        public AutoDraw autoDraw;
+
+        /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+
+        Color _color;
 
         /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
 
         void Update(){
 
-            for ( int i=0; i<startEnd.Length; i++ ){
+            if ( !auto ){
 
-                startEnd[ i ].name = $"{ startEnd[ i ].start.name } and { startEnd[ i ].end.name }";
+                for ( int i=0; i<startEnd.Length; i++ ){
 
-                startEnd[ i ].color = new Color( startEnd[ i ].color.r, startEnd[ i ].color.g, startEnd[ i ].color.b, 255 );
-                Debug.DrawLine( startEnd[ i ].start.transform.position, startEnd[ i ].end.transform.position, startEnd[ i ].color, startEnd[ i ].interval );
+                    startEnd[ i ].name = $"{ startEnd[ i ].start.name } and { startEnd[ i ].end.name }";
+
+                    startEnd[ i ].color = new Color( startEnd[ i ].color.r, startEnd[ i ].color.g, startEnd[ i ].color.b, 255 );
+                    Debug.DrawLine( startEnd[ i ].start.transform.position, startEnd[ i ].end.transform.position, startEnd[ i ].color, startEnd[ i ].interval );
+
+                }
+
+            }
+            else {
+
+                GameObject[] objs = GameObject.FindGameObjectsWithTag( autoDraw.end );
+
+                for ( int i=0; i<objs.Length; i++ ){
+
+                    autoDraw.name = $"{ autoDraw.start.name } and { autoDraw.end } Tag";
+
+                    if ( Vector2.Distance( autoDraw.start.transform.position, objs[ i ].transform.position ) <= autoDraw.dis ){
+
+                        _color = new Color( autoDraw.disColor.r, autoDraw.disColor.g, autoDraw.disColor.b, 255 );
+
+                    }
+                    else {
+
+                        _color = new Color( autoDraw.color.r, autoDraw.color.g, autoDraw.color.b, 255 );
+
+                    }
+
+                    Debug.DrawLine( autoDraw.start.transform.position, objs[ i ].transform.position, _color, autoDraw.interval );
+
+                }
 
             }
             
